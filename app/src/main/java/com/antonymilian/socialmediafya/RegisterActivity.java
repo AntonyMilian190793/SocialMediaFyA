8559e16316241e226be4ea013bfb9502552234ee
AntonyMilian190793 <antonymilian19@gmail.com>
2022-10-17 11:38:04 -0500

    17-10Ã-2022

--- app/src/main/java/com/antonymilian/socialmediafya/RegisterActivity.java
+++ app/src/main/java/com/antonymilian/socialmediafya/RegisterActivity.java
@@ -3,12 +3,26 @@ package com.antonymilian.socialmediafya;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    CircleImageView mCircleImageViewBack;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mCircleImageViewBack = findViewById(R.id.cicleImageBack);
+        mCircleImageViewBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
