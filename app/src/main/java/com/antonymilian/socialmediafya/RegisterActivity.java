0f103b89d73264d37dfb13462c83424a7e53b040
AntonyMilian190793 <51470801+AntonyMilian190793@users.noreply.github.com>
2022-10-17 18:39:41 -0500

    17-10-2022

--- app/src/main/java/com/antonymilian/socialmediafya/RegisterActivity.java
+++ app/src/main/java/com/antonymilian/socialmediafya/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.antonymilian.socialmediafya;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,7 +8,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -23,6 +28,7 @@ public class RegisterActivity extends AppCompatActivity {
     TextInputEditText mTextInputConfirmPassword;
     Button mButtonRegister;
 
+    FirebaseAuth mAuth;
 
 
 
@@ -32,11 +38,13 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         mCircleImageViewBack = findViewById(R.id.cicleImageBack);
-        mTextInputUsername = findViewById(R.id.textInputEmail);
-        mTextInputEmail = findViewById(R.id.textInputEmail);
-        mTextInputPassword = findViewById(R.id.textInputPassword);
-        mTextInputConfirmPassword = findViewById(R.id.textInputConfirmPassword);
+        mTextInputUsername = findViewById(R.id.textInputUserNameR);
+        mTextInputEmail = findViewById(R.id.textInputEmailR);
+        mTextInputPassword = findViewById(R.id.textInputPasswordR);
+        mTextInputConfirmPassword = findViewById(R.id.textInputConfirmPasswordR);
         mButtonRegister = findViewById(R.id.btnRegister);
+
+        mAuth = FirebaseAuth.getInstance();
 
         mButtonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,6 +69,15 @@ public class RegisterActivity extends AppCompatActivity {
 
         if(!username.isEmpty() && !email.isEmpty() && !password.isEmpty() && !confirmPassword.isEmpty()){
             if(isEmailValid(email)){
+                if(password.equals(confirmPassword)){
+                    if(password.length() >= 6){
+                        createUser(email, password);
+                    }else{
+                        Toast.makeText(this, "La contraseña debe tener al menos 6 caracteres!", Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(this, "Las contraseñas no cinciden!", Toast.LENGTH_SHORT).show();
+                }
                 Toast.makeText(this, "Insertaste todos los campos y el email es valido!", Toast.LENGTH_SHORT).show();
             }else{
                 Toast.makeText(this, "Insertatse todos los campos pero el email no es valido!", Toast.LENGTH_LONG).show();
@@ -69,6 +86,19 @@ public class RegisterActivity extends AppCompatActivity {
         }else{
             Toast.makeText(this, "Para continuar inserta todos los campos!", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void createUser(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(RegisterActivity.this, "El usuario se registro correctamente!", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(RegisterActivity.this, "No se pudo registrar el usuario!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     public boolean isEmailValid(String email) {
