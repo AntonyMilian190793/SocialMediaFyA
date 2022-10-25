c2f07e9bff426d3c6145703280f25ac7c6f60bf4
AntonyMilian190793 <antonymilian19@gmail.com>
2022-10-25 15:31:37 -0500

    25-10-2022

---
+++ app/src/main/java/com/antonymilian/socialmediafya/activities/PostActivity.java
@@ -0,0 +1,16 @@
+package com.antonymilian.socialmediafya.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.antonymilian.socialmediafya.R;
+
+public class PostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+    }
+}
