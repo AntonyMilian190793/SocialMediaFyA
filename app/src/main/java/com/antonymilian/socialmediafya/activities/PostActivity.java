f03fb384aa61e8a80310c474eac9ba7d3c81884e
AntonyMilian190793 <antonymilian19@gmail.com>
2022-10-27 11:17:12 -0500

    27-10-2022

--- app/src/main/java/com/antonymilian/socialmediafya/activities/PostActivity.java
+++ app/src/main/java/com/antonymilian/socialmediafya/activities/PostActivity.java
@@ -1,16 +1,61 @@
 package com.antonymilian.socialmediafya.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.antonymilian.socialmediafya.R;
+import com.antonymilian.socialmediafya.utils.FileUtil;
+
+import java.io.File;
 
 public class PostActivity extends AppCompatActivity {
+
+    ImageView mImagenViewPost1;
+    File mImageFile;
+    private final int GALLERY_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        mImagenViewPost1 = findViewById(R.id.imageViewPost1);
+        mImagenViewPost1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGalLery();
+            }
+        });
+
+    }
+
+    private void openGalLery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GALLERY_REQUEST_CODE && resultCode == RESULT_OK){
+            try {
+                mImageFile = FileUtil.from(this, data.getData());
+                mImagenViewPost1.setImageBitmap(BitmapFactory.decodeFile(mImageFile.getAbsolutePath()));
+            }catch (Exception e){
+                Log.d("ERROR", "Se produjo un error!" + e.getMessage());
+                Toast.makeText(this, "Se produjo un error" + e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
