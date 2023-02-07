6b2cb08e31819a71f78d46f8ad85fc157f8f176f
AntonyMilian190793 <antonymilian19@gmail.com>
2023-02-07 11:47:18 -0500

    707-02-2023

--- app/src/main/java/com/antonymilian/socialmediafya/activities/PostDetailActivity.java
+++ app/src/main/java/com/antonymilian/socialmediafya/activities/PostDetailActivity.java
@@ -2,6 +2,9 @@ package com.antonymilian.socialmediafya.activities;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,14 +12,18 @@ import com.antonymilian.socialmediafya.R;
 import com.antonymilian.socialmediafya.adapters.SliderAdapter;
 import com.antonymilian.socialmediafya.models.SliderItem;
 import com.antonymilian.socialmediafya.providers.PostProvider;
+import com.antonymilian.socialmediafya.providers.UsersProvider;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
 import com.smarteist.autoimageslider.SliderAnimations;
 import com.smarteist.autoimageslider.SliderView;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class PostDetailActivity extends AppCompatActivity {
 
@@ -24,8 +31,18 @@ public class PostDetailActivity extends AppCompatActivity {
     SliderAdapter mSliderAdapter;
     List<SliderItem> mSliderItems = new ArrayList<>();
     PostProvider mPostProvider;
+    UsersProvider mUsersProvider;
 
     String mExtraPostId;
+
+    TextView mTextViewTitle;
+    TextView mTextViewDescription;
+    TextView mTextViewUsername;
+    TextView mTextViewPhone;
+    TextView mTextViewNameCategory;
+    ImageView mImageViewCategory;
+    CircleImageView mCircleImageViewProfile;
+    Button mButtonShowProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +50,18 @@ public class PostDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_post_detail);
 
         mSliderView = findViewById(R.id.imageSlider);
+
+        mTextViewTitle = findViewById(R.id.textViewTitle);
+        mTextViewDescription = findViewById(R.id.textViewDescription);
+        mTextViewUsername = findViewById(R.id.textViewUsername);
+        mTextViewPhone = findViewById(R.id.textViewPhone);
+        mTextViewNameCategory = findViewById(R.id.textViewNameCategory);
+        mImageViewCategory = findViewById(R.id.imageViewCategory);
+        mCircleImageViewProfile = findViewById(R.id.circleImageProfile);
+        mButtonShowProfile = findViewById(R.id.btnShowProfile);
+
         mPostProvider = new PostProvider();
+        mUsersProvider = new UsersProvider();
 
         mExtraPostId = getIntent().getStringExtra("id");
 
@@ -72,9 +100,61 @@ public class PostDetailActivity extends AppCompatActivity {
                         mSliderItems.add(item);
                     }
 
+                    if(documentSnapshot.contains("title")){
+                        String title = documentSnapshot.getString("title");
+                        mTextViewTitle.setText(title.toUpperCase());
+                    }
+                    if(documentSnapshot.contains("descripcion")){
+                        String descripcion = documentSnapshot.getString("descripcion");
+                        mTextViewDescription.setText(descripcion);
+                    }
+                    if(documentSnapshot.contains("category")){
+                        String category = documentSnapshot.getString("category");
+                        mTextViewNameCategory.setText(category);
+
+                        if(category.equals("Noticias")){
+                            mImageViewCategory.setImageResource(R.drawable.ic_noticias);
+                        }else if(category.equals("Informe")){
+                            mImageViewCategory.setImageResource(R.drawable.ic_informe);
+                        }else if(category.equals("Colegios")){
+                            mImageViewCategory.setImageResource(R.drawable.ic_colegio);
+                        }else if(category.equals("Otros")){
+                            mImageViewCategory.setImageResource(R.drawable.ic_otros);
+                        }
+                    }
+                    if(documentSnapshot.contains("idUser")){
+                        String idUser = documentSnapshot.getString("idUser");
+                        getUserInfo(idUser);
+                    }
+
+
                     instanceSlider();
                 }
             }
         });
+    }
+
+    private void getUserInfo(String idUser){
+        mUsersProvider.getUser(idUser).addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if(documentSnapshot.exists()){
+                    if(documentSnapshot.contains("username")){
+                        String username = documentSnapshot.getString("username");
+                        mTextViewUsername.setText(username);
+                    }
+                    if(documentSnapshot.contains("phone")){
+                        String phone = documentSnapshot.getString("phone");
+                        mTextViewPhone.setText(phone);
+                    }
+                    if(documentSnapshot.contains("image_profile")){
+                        String imagePrfile = documentSnapshot.getString("image_profile");
+                        Picasso.with(PostDetailActivity.this).load(imagePrfile).into(mCircleImageViewProfile);
+                    }
+                }
+
+            }
+        });
+
     }
 }
