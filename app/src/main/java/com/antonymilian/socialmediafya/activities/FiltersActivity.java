724e2999aa46b72efde4fcae360e4bc5240eda76
AntonyMilian190793 <antonymilian19@gmail.com>
2023-02-15 09:24:34 -0500

    15-02-2023

--- app/src/main/java/com/antonymilian/socialmediafya/activities/FiltersActivity.java
+++ app/src/main/java/com/antonymilian/socialmediafya/activities/FiltersActivity.java
@@ -1,15 +1,32 @@
 package com.antonymilian.socialmediafya.activities;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.antonymilian.socialmediafya.R;
+import com.antonymilian.socialmediafya.adapters.PostsAdapter;
+import com.antonymilian.socialmediafya.models.Post;
+import com.antonymilian.socialmediafya.providers.AuthProvider;
+import com.antonymilian.socialmediafya.providers.PostProvider;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.Query;
 
 public class FiltersActivity extends AppCompatActivity {
 
     String mExtraCategory;
+
+    AuthProvider mAuthProvider;
+    RecyclerView mRecyclerView;
+    PostProvider mPostProvider;
+    PostsAdapter mPostAdapter;
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +34,43 @@ public class FiltersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_filters);
 
         mExtraCategory = getIntent().getStringExtra("category");
+
+        mRecyclerView = findViewById(R.id.recycleViewFilter);
+        mAuthProvider = new AuthProvider();
+        mPostProvider = new PostProvider();
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Filtros");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(FiltersActivity.this);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+
         Toast.makeText(this, "La categoria que seleccionó es " + mExtraCategory, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Query query = mPostProvider.getPostByCategoryAndTimestamp(mExtraCategory);
+        FirestoreRecyclerOptions<Post> options = new FirestoreRecyclerOptions.Builder<Post>().setQuery(query, Post.class).build();
+        mPostAdapter = new PostsAdapter(options, FiltersActivity.this);
+        mRecyclerView.setAdapter(mPostAdapter);
+        mPostAdapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mPostAdapter.stopListening();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId() == android.R.id.home){
+            finish();
+        }
+        return true;
     }
 }
