84aa1f72c79e9a88c07b62f2cb748e0040f9184d
AntonyMilian190793 <antonymilian19@gmail.com>
2023-02-27 13:45:06 -0500

    27-02-2023

--- app/src/main/java/com/antonymilian/socialmediafya/services/MyFirebaseMeesagingClient.java
+++ app/src/main/java/com/antonymilian/socialmediafya/services/MyFirebaseMeesagingClient.java
@@ -1,9 +1,13 @@
 package com.antonymilian.socialmediafya.services;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
 
+import com.antonymilian.socialmediafya.channel.NotificationHelper;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
 
 public class MyFirebaseMeesagingClient extends FirebaseMessagingService {
 
@@ -15,5 +19,18 @@ public class MyFirebaseMeesagingClient extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
+        Map<String, String> data = remoteMessage.getData();
+        String title = data.get("title");
+        String body = data.get("body");
+
+        if(title != null){
+            showNotification(title, body);
+        }
+    }
+
+    private void showNotification(String title, String body){
+        NotificationHelper notificationHelper = new NotificationHelper(getBaseContext());
+        NotificationCompat.Builder builder = notificationHelper.getNotification(title, body);
+        notificationHelper.getManager().notify(1, builder.build());
     }
 }
