89fab3bc4f54c20574396da9ee0fbf049ba6994b
AntonyMilian190793 <antonymilian19@gmail.com>
2023-02-28 13:38:56 -0500

    28-02-2023

--- app/src/main/java/com/antonymilian/socialmediafya/activities/ChatActivity.java
+++ app/src/main/java/com/antonymilian/socialmediafya/activities/ChatActivity.java
@@ -1,8 +1,13 @@
 package com.antonymilian.socialmediafya.activities;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.antonymilian.socialmediafya.R;
 import com.antonymilian.socialmediafya.models.Chat;
@@ -15,17 +20,31 @@ public class ChatActivity extends AppCompatActivity {
     String mExtraIdUser1;
     String mExtraIdUser2;
     ChatsProvider mChatsProvider;
+    View mActionBarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        showCustomToolbar(R.layout.custom_chat_toolbar);
 
         mExtraIdUser1 = getIntent().getStringExtra("idUser1");
         mExtraIdUser2 = getIntent().getStringExtra("idUser2");
         mChatsProvider = new ChatsProvider();
 
         createChat();
+    }
+
+    private void showCustomToolbar(int resource) {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("");
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayShowCustomEnabled(true);
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mActionBarView = inflater.inflate(resource, null);
+        actionBar.setCustomView(mActionBarView);
     }
 
     private void createChat() {
